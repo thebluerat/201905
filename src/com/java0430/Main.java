a78081b0195e22402a91314a21a8a2c01113ac8c
GD3 <GD3@DESKTOP-9HHTEFM>
2019-04-30 16:44:11 +0900

    밍먕

---
+++ src/com/java0430/Main.java
@@ -0,0 +1,21 @@
+package com.java0430;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Main") // URL : http://localhost:8080/201905/Main을 정의한 것.
+public class Main extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
