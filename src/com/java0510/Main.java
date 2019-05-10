66b10ff657788f171798c6009ed2886edca145f9
GD3 <GD3@DESKTOP-SL3939K>
2019-05-10 18:20:46 +0900

    구구단

---
+++ src/com/java0510/Main.java
@@ -0,0 +1,12 @@
+package com.java0510;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Times 구구 = new Times();
+		구구.times();
+
+	}
+
+}
---
+++ src/com/java0510/Times.java
@@ -0,0 +1,14 @@
+package com.java0510;
+
+public class Times {
+	
+	public void times() {
+		
+	for( int i = 1; i < 10; i++) {
+		System.out.println(i + "단");
+		for(int j = 1; j < 10; j++) {
+			System.out.println(i + " * " + j + " = " + i*j);
+			}
+		}System.out.println();
+	}
+}
